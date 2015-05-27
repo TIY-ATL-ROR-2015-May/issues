7aa43cdf15aead8c42c45c494c68f072967191c2
Brit Butler <redline6561@gmail.com>
2015-05-27 11:34:35 -0400

    Add support for getting team by name and getting gist contents.

--- lib/issues.rb
+++ lib/issues.rb
@@ -18,6 +18,24 @@ module Issues
     def list_members(team_id)
       self.class.get("/teams/#{team_id}/members")
     end
+
+    def get_team_by_name(org_id, team_name)
+      teams = self.list_teams(org_id)
+      team = teams.find { |x| x['name'] == team_name }
+      self.list_members(team['id'])
+    end
+
+    def get_gist(gist_id)
+      self.class.get("/gists/#{gist_id}")
+    end
+
+    def get_gist_content(gist_id)
+      response = self.get_gist(gist_id)
+      response['files'].values.first['content']
+    end
+
+    def create_issue
+    end
   end
 end
 
