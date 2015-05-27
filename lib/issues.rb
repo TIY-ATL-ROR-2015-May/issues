24ab02b95082ffcd8c79c0568bc3a6083d365a5e
Brit Butler <redline6561@gmail.com>
2015-05-27 10:26:09 -0400

    Add Github class with basic auth and some simple GET methods.

--- lib/issues.rb
+++ lib/issues.rb
@@ -1,5 +1,24 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'pry'
+require 'httparty'
+
 require "issues/version"
 
 module Issues
-  # Your code goes here...
+  class Github
+    include HTTParty
+    base_uri 'https://api.github.com'
+    basic_auth ENV['GH_USER'], ENV['GH_PASS']
+
+    def list_teams(org_id)
+      self.class.get("/orgs/#{org_id}/teams")
+    end
+
+    def list_members(team_id)
+      self.class.get("/teams/#{team_id}/members")
+    end
+  end
 end
+
+binding.pry
